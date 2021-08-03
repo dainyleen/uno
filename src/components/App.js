f8d2012d06cd815ffcd4709d9c1b5100e93b8dd3
Dainyleen Masangkay <dainyleen@gmail.com>
2021-08-03 23:39:57 +1200

    Imported Header component to App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from 'react'
+
+import Header from './Header'
 
 class Counter extends React.Component {
   state = { 
     score: 0 
-  };
+  }
   
   incrementScore = () => {
     this.setState( prevState => ({
       score: prevState.score + 1
-    }));
+    }))
   }
 
   decrementScore = () => {
     this.setState( prevState => ({
       score: prevState.score - 1
-    }));
+    }))
   }
 
   render() {
@@ -24,7 +26,7 @@ class Counter extends React.Component {
         <span className="counter-score">{ this.state.score }</span>
         <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
-    );
+    )
   }
 }
   
@@ -38,7 +40,7 @@ const Player = (props) => {
 
       <Counter />
     </div>
-  );
+  )
 }
 
 class App extends React.Component {
@@ -61,14 +63,14 @@ class App extends React.Component {
         id: 4
       }
     ]
-  };
+  }
 
   handleRemovePlayer = (id) => {
     this.setState( prevState => {
       return {
         players: prevState.players.filter(p => p.id !== id)
-      };
-    });
+      }
+    })
   }
 
   render() {
@@ -89,8 +91,8 @@ class App extends React.Component {
           />
         )}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
       <h1>{ props.title }</h1>
       <span className="stats">Players: {props.totalPlayers}</span> 
     </header>
-  );
+  )
 }
 
 export default Header
