4fa09bb5ddc024b7d7f70df5a6d77bbf757638d8
Dainyleen Masangkay <dainyleen@gmail.com>
2021-08-04 00:36:30 +1200

    Imported Stats component to Header.js file

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Stats = (props) => {
   return (
-    
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>0</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>0</td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
