e5c693f1664de742667d653797d8fffdb6acc9f5
Dainyleen Masangkay <dainyleen@gmail.com>
2021-08-04 17:43:46 +1200

    Imported Stopwatch component into Header.js file

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 class Stopwatch extends Component {
   render() {
     return (
-      
+      <div className='stopwatch'>
+        <h2>Stopwatch</h2>
+        <span className='stopwatch-time'>0</span>
+        <button>Start</button>
+        <button>Reset</button>
+      </div>
     )
   }
 }
